48dcf3843ed28af2062a852049557dc629714c34
Viktor Sec <hey@viktorsec.com>
2017-05-21 23:29:17 +0200

    Actual fix

--- camera.sh
+++ camera.sh
@@ -4,4 +4,4 @@ DATE=$(date +"%Y-%m-%d_%H%M")
 
 fswebcam -r 1280x720 --no-banner /home/pi/Documents/greenlapse/photos/$DATE.jpg
 
-scp -i /home/pi/.ssh/id_kootchawnet /home/pi/Documents/greenlapse/photos/`ls -ltr /home/pi/Documents/greenlapse/photos/*.jpg | tail -1 | cut -d ' ' -f 9` viktor@kootchaw.net:/srv/httpd/habitat.viktorsec.com/recent.jpg
+scp -i /home/pi/.ssh/id_kootchawnet `ls -ltr /home/pi/Documents/greenlapse/photos/*.jpg | tail -1 | cut -d ' ' -f 9` viktor@kootchaw.net:/srv/httpd/habitat.viktorsec.com/recent.jpg
